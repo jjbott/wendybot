7aa368a7ae6ee32352b7a98fc211e943ff233f85
joseph.bott <joseph.bott@streetlinks.com>
2014-04-01 09:56:45 -0500

    Removing all polling code

--- wendybot.js
+++ wendybot.js
@@ -21,17 +21,6 @@ var TIMER = 30000;
 // Port to listen for updates
 var PORT = 1337;
 
-// example object representing a build
-var build_object = 
-  {
-		jobname: 'TRUNK',
-		valid: true,
-		number: 0,
-		inProgress: false
-	};
-
-// map containing builds to monitor
-var buildmap = {};
 
 var jenkins = jenkinsapi.init(JENKINS_URL);
 
@@ -48,193 +37,7 @@ var daJerk = jerk( function( j ) {
 	j.watch_for( /^(.*)$/, function(message) {
 		last_said[message.user] = message.match_data[1];
 	});
-
-	// timed status monitor on 'build' jobs
-	j.watch_for( '^!monitor (.*)', function( message ) {
-		console.log('#### call to monitor job ####');
-
-		var job = message.match_data[1];
-
-		console.log('monitor build status: ' + job);
-
-		if (buildmap[job] != null && buildmap[job].valid == false) {
-			message.say('--- Unknown Jenkins job: ' + job + ' ---');
-			return;
-		}
-
-		// get build
-		jenkins.last_build_info(job, function(error, data) {
-
-			if (error == true ) {
-				// job does not exist
-				message.say('--- Unknown Jenkins job: ' + job + ' ---');
-
-				buildmap[job] = 
-					{
-						jobname: job,
-						valid: false,
-						number: 0,
-						inProgress: false
-					}
-
-				return;
-			} else if (buildmap[job] != null && buildmap[job].valid == true ) {
-				// job is already monitored
-				message.say('--- Already monitoring job: ' + job + ' ---');
-			} else {
-				// start monitoring this job
-				message.say('--- Start monitoring job: ' + job + ' ---');
-
-				buildmap[job] = {
-					jobname: job,
-					valid: true,
-					number: data.number,
-					inProgress: false
-				}
-
-				// kick off the timer initially
-				setInterval( function() {
-					jenkins.build_info(job, buildmap[job].number, function(error, data) {
-
-						if (error == null && data.statusCode == null) { // no statusCode means its not a 404
-							var result = data.result;
-							if ( result == 'UNSTABLE' ) {
-								result = color.bold.pink(result);
-							}
-							if ( result == 'FAILURE' ) {
-								result = color.bold.underline.red(result);
-							}
-							if ( result == 'SUCCESS' ) {
-								result = color.green(result);
-							}
-
-							var description = data.actions[0].causes[0].shortDescription;
-							var url = data.url;
-							console.log(url);
-							
-							var branch = getLastBuiltRevision(data).branch[0].name;
-
-							// build is in progress, mark it as in progress
-							if (result == null) {
-								var msgtext = '--- Jenkins build ' + job + ' ' + buildmap[job].number + 
-									' IN PROGRESS. ' + description + ' | ' + url + ' ---' + branch;
-
-								console.log(msgtext);
-
-								if (buildmap[job].inProgress == false) {
-									//message.say(msgtext);
-									buildmap[job].inProgress = true;
-								}
-
-								return;
-							}
-
-							var builtOn = data.builtOn;
-
-							// var msgtext = '--- Jenkins build ' + job + ' ' + result + ' ' +
-								// 'on ' + builtOn + '. ' + description + ' | ' + url + ' ---' + branch;
-							var msgtext = 'Branch ' + branch + " build #" + data.number + ':' + result + ' in ' + Math.floor(data.duration / 60000) + ' min ';
-							var secs = Math.floor(data.duration % 60000 / 1000);
-							if ( secs > 0 ) {
-								msgtext += secs + ' sec: ';
-							}
-							else {
-								msgtext += ': ';
-							}
-							msgtext += data.url;
-							
-							message.say(msgtext);
-
-							buildmap[job].number = buildmap[job].number + 1;
-
-							buildmap[job].inProgress = false;
-
-						} else {
-							console.log('--- ' + 'waiting on next ' + job + ' job ' + buildmap[job].number + ' in ' + (TIMER / 1000) + ' seconds ---');
-						}
-					});
-				}, TIMER);
-			}	
-
-		});
-	});
-
-	// one time status check on 'build' job
-	j.watch_for( '^!check (.*)', function( message ) {
-		console.log('#### call to check job ####');
-
-		var job = message.match_data[1];
 		
-		console.log('check build status: ' + job);
-
-		jenkins.last_build_info(job, function(error, data) {
-
-			if (error == true) {
-				message.say('--- Unknown job: ' + job);
-				return;
-			}
-
-			var result = data.result;
-			if (building == true) {
-				result = 'IN PROGRESS';
-			}
-			
-			if ( result == 'UNSTABLE' ) {
-				result = color.bold.pink(result);
-			}
-			if ( result == 'FAILURE' ) {
-				result = color.bold.underline.red(result);
-			}
-			if ( result == 'SUCCESS' ) {
-				result = color.bold.green(result);
-			}
-
-			var description = data.actions[0].causes[0].shortDescription;
-			var builtOn = data.builtOn;
-			var url = data.url;
-			var number = data.number;
-
-			// var msgtext = '--- Jenkins build ' + job + ' ' + result + ' ' +
-				// 'on ' + builtOn + '. ' +  description + ' | ' + url + ' ---';
-				
-			
-			var msgtext = 'Branch ' + branch + " build #" + data.number + ':' + result;
-			if ( building == true )
-			{
-				msgtext += ' Est. duration ' + Math.floor(estimatedDuration.duration / 60000) + ' min ';
-				var secs = Math.floor(estimatedDuration.duration % 60000 / 1000);
-				if ( secs > 0 ) {
-					msgtext += secs + ' sec: ';
-				}
-				else {
-					msgtext += ': ';
-				}
-			}
-			else {
-				msgtext += ' in ' + Math.floor(data.duration / 60000) + ' min ';
-				var secs = Math.floor(data.duration % 60000 / 1000);
-				if ( secs > 0 ) {
-					msgtext += secs + ' sec: ';
-				}
-				else {
-					msgtext += ': ';
-				}
-			}
-			
-			msgtext += data.url;
-			
-			console.log(data);
-			console.log('IRC-msg: ' + msgtext);    
-			message.say(msgtext);
-		});
-	});
-
-	// display help info
-	j.watch_for('!help', function( message ) {
-		message.say('--- !check [JOBNAME] > performs a one time check on Jenkins job [JOBNAME] ---');
-		message.say('--- !monitor [JOBNAME] > periodically monitors Jenkins job [JOBNAME] ---');
-	});
-	
 	var replaceLastSaid = function(user, pattern, modifier, newString, say) {
 		if (!last_said[user]) {
 			return;
@@ -268,7 +71,7 @@ var daJerk = jerk( function( j ) {
 	j.watch_for( new RegExp('^' + options.nick + ' status (([^\\"\\s]+)|\\"(.+)\\") (\\d+)$'), function(message) {
 		buildFinished(message.match_data[2] || message.match_data[3], message.match_data[4], message);
 	});
-
+	
 }).connect( options );
 
 var say = function(text, message, delay) {
@@ -356,26 +159,9 @@ var sayBuildStatus = function(jobName, buildNumber, errors, message) {
 			console.log(url);
 			
 			var branch = getLastBuiltRevision(data).branch[0].name;
-	/*
-			// build is in progress, mark it as in progress
-			if (result == null) {
-				var msgtext = '--- Jenkins build ' + job + ' ' + buildmap[job].number + 
-					' IN PROGRESS. ' + description + ' | ' + url + ' ---' + branch;
 
-				console.log(msgtext);
-
-				if (buildmap[job].inProgress == false) {
-					//message.say(msgtext);
-					buildmap[job].inProgress = true;
-				}
-
-				return;
-			}
-	*/
 			var builtOn = data.builtOn;
 
-			// var msgtext = '--- Jenkins build ' + job + ' ' + result + ' ' +
-				// 'on ' + builtOn + '. ' + description + ' | ' + url + ' ---' + branch;
 			var msgtext = 'Branch ' + branch + " build #" + data.number + ':' + result + ' in ' + Math.floor(data.duration / 60000) + ' min ';
 			var secs = Math.floor(data.duration % 60000 / 1000);
 			if ( secs > 0 ) {
